03b97bf140c70c5088b6b1b6abe45570ee4367c1
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2022-07-03 11:45:08 -0400

    Text to speech, saving individual lines to file.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import re
 
 from googletrans import Translator
+from gtts import gTTS
 
 
 SRC_LANG = 'es'
@@ -15,8 +16,9 @@ def main():
     # Read text to translate.
     src_text = "Me llamo Miguel, y me gusta leer. Pero no me gusta libros sobre gatos."
 
-    # Translate test text.
+    # Translate text one line at a time.
     translator = Translator()
+    i = 0
     for src_line in re.split("\.|!|\?", src_text):
 
         # Skip empty lines.
@@ -24,10 +26,21 @@ def main():
         if len(src_line) == 0:
             continue
 
-        print(f"{SRC_LANG}: {src_line}")
+        # Translate line to target language.
         dest_line = translator.translate(src_line, src=SRC_LANG, dest=DEST_LANG).text
+
+        # Convert line (in both languages) to speech.
+        src_speech = gTTS(src_line, lang=SRC_LANG)
+        dest_speech = gTTS(dest_line, lang=DEST_LANG)
+
+        # Display text and play audio for source and target lines.
+        # TEMP: Saving audio to file individually.
+        print(f"{SRC_LANG}: {src_line}")
+        src_speech.save(f"src_{i}.mp3")
         print(f"{DEST_LANG}: {dest_line}")
+        dest_speech.save(f"dest_{i}.mp3")
         print("")
+        i += 1
 
 
 if __name__ == "__main__":
